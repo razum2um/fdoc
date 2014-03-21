12d1bb637fb8b3976adb9f045471f54a5a232177
Vlad Bokov <razum2um@mail.ru>
2014-03-21 21:10:07 +0700

    http auth at fdoc::server

--- lib/fdoc/server.rb
+++ lib/fdoc/server.rb
@@ -1,11 +1,25 @@
 require 'sinatra/base'
+require 'rack'
 require 'rack/contrib/try_static'
 
 module Fdoc
-  class Server < Sinatra::Base
-    use Rack::TryStatic,
-     :root => "#{::Rails.root}/public/fdoc",  # static files root dir
-     :urls => %w[/],     # match all requests
-     :try => ['.html', 'index.html', '/index.html'] # try these postfixes sequentially
+  class Server
+    def self.to_rack(options = {})
+      default_path = options[:path] || 'lurker'
+
+      Class.new(Sinatra::Base) do
+
+        if (username, password = options.values_at(:username, :password)).all?(&:present?)
+          use Rack::Auth::Basic, "Protected Area" do |u, p|
+            username == u && password == p
+          end
+        end
+
+        use Rack::TryStatic,
+         :root => "#{::Rails.root}/#{default_path}",  # static files root dir
+         :urls => %w[/],     # match all requests
+         :try => ['.html', 'index.html', '/index.html'] # try these postfixes sequentially
+      end
+    end
   end
 end
